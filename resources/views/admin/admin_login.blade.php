52210c0e3c8e650a7b6e21e3a84a79a9f9fcd97c
batkobatko <novakov_sasa@yahoo.com>
2020-06-19 12:03:17 +0200

    #16 Sections Module in Admin Panel (I)- Migration - Seeding (section table)

--- resources/views/admin/admin_login.blade.php
+++ resources/views/admin/admin_login.blade.php
@@ -21,7 +21,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href=" {{ asset('/') }}"><b>Admin</b>LTE</a>
+    <a href=" {{ asset('/') }}"><b>Admin</b>Panel</a>
   </div>
   <!-- /.login-logo -->
   <div class="card">
@@ -64,15 +64,14 @@
             </div>
           </div>
         </div>
-        <div class="row">
-          <div class="col-8">
+          <!-- <div class="col-8">
             <div class="icheck-primary">
               <input type="checkbox" id="remember">
               <label for="remember">
                 Remember Me
               </label>
             </div>
-          </div>
+          </div>-->
           <!-- /.col -->
           <div class="col-4">
             <button type="submit" class="btn btn-primary btn-block">Sign In</button>
@@ -80,18 +79,6 @@
           <!-- /.col -->
         </div>
       </form>
-
-
-      <!-- /.social-auth-links -->
-
-      <p class="mb-1">
-        <a href="forgot-password.html">I forgot my password</a>
-      </p>
-      <p class="mb-0">
-        <a href="register.html" class="text-center">Register a new membership</a>
-      </p>
-    </div>
-    <!-- /.login-card-body -->
   </div>
 </div>
 <!-- /.login-box -->
