ad65a83572c7d0192671ee331875ad152b746eff
batkobatko <novakov_sasa@yahoo.com>
2020-06-19 11:29:35 +0200

    #15 Update Admin Sidebar (720p_6fps_H264-128kbit_AAC), create migration for section

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 
 use Illuminate\Database\Seeder;
 
@@ -11,7 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-    	$this->call(AdminsTableSeeder::class);
-        // $this->call(UserSeeder::class);
+    	// $this->call(AdminsTableSeeder::class);
+        // $this->call(AdminsTableSeeder::class);
+        $this->call(SectionsTableSeeder::class);
     }
 }
